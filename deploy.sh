92b7fec8028b60b3177e051eada6abbf3fbc3c9c
Raphael Okure <raphael@arccosgolf.com>
2018-05-29 04:38:33 -0700

    * update deploy scrpt

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@
 function deploy {
     if [ -z $1 ]
     then 
-        echo '* Enter version number: See package.json for current (and increment before deploying)!'
+        echo "* Enter version number: See package.json (increment before deploying)!"
         return 5
     else
         # Notify begin
-        echo Deploying version $1 &&
+        echo "** Building version $1" &&
         # compile JS
         npm run build && 
+        # Notify Git actions
+        echo "** Tag $1 and commit changes to git..." &&
         # Add working files
         git add . && 
         # Tag  current version
@@ -18,6 +20,8 @@ function deploy {
         git commit -m "** : AUTOCOMMIT : tag $1" &&
         # push to github
         git push -u --tags && 
+        # Notify NPM Actions
+        echo "Publishing $1 to NPM..." &&
         # publish to npm
         npm publish 
     fi
