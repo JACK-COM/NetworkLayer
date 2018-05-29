9e180f35ff327ba97ae718ad2144a255378e2512
Raphael Okure <raphael@arccosgolf.com>
2018-05-29 04:35:49 -0700

    ** : AUTOCOMMIT : tag 0.4.1

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,30 @@
 #!//bin/bash
 
-# compile JS
-npm run build && 
+function deploy {
+    if [ -z $1 ]
+    then 
+        echo '* Enter version number: See package.json for current (and increment before deploying)!'
+        return 5
+    else
+        # Notify begin
+        echo Deploying version $1 &&
+        # compile JS
+        npm run build && 
+        # Add working files
+        git add . && 
+        # Tag  current version
+        git tag $1 && 
+        # commit
+        git commit -m "** : AUTOCOMMIT : tag $1" &&
+        # push to github
+        git push -u --tags && 
+        # publish to npm
+        npm publish 
+    fi
 
-# publish to npm
-npm publish && 
+    return 0
+}
 
-# push to github
-git push -u
+deploy $1
+
+# deploy
