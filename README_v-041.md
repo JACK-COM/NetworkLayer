f6697c86e14f4b1568e925c546c71654f2cf1d77
RaphJack <info@mrjackdaw.com>
2021-05-11 18:07:44 -0700

    * bamp to 060   * splits core file into separate documents   * updates package dependencies   * STILL doesn't add any new dependency   * improves test coverage   * brings less shame to my lineage

---
+++ README_v-041.md
@@ -0,0 +1,95 @@
+# Application Network Request Layer (v.0.4.1)
+
+## Table Of Contents
+1. [What is it?](#what-is-it)
+2. [How do you use it?](#how-do-you-use-it)
+3. [API](#api-available-via-import)
+4. [Terminology](#terminology)
+5. [FAQs](#faqs)
+
+## What is it?
+An abstraction layer for handling network requests/responses. 
+
+## How do you use it?
+### Installation
+    npm i --save @jackcom/app-network-layer
+
+### Usage
+```javascript
+import NetworkLayer from '@jackcom/app-network-layer'; 
+
+// Define your endpoints in an object (the most basic example of a route is below).
+// Endpoints/Routes require certain keys: see `RouteDefinition` interface below
+const METHODS = NetworkLayer.METHODS; // GET, POST, DELETE, PUT, PATCH
+const endpoints = {
+    getUsers: {
+        url: params => `https://my.api.domain/${params.userId}`,
+        method: METHODS.GET
+    }
+}
+
+// Instantiate your layer
+const APIConfig = NetworkLayer.APIConfig;
+const config = new APIConfig(endpoints); 
+
+// Use it with an endpoint:
+config
+    .getUsers({ userId: mySource.userId }) // returns a Promise object
+    .then( ... )
+
+// OR
+config
+    .request("getUsers")
+    .with({ userId: mySource.userId }) // returns a Promise object
+    .then( ... ) 
+```
+
+## API (available via import):
+Note that although this guide uses **NetworkLayer** for clarity, the default export is not named.
+* `NetworkLayer`: default library export 
+* `NetworkLayer.APIConfig`: the class to instantiate: [route handler](#apiconfig-class) and promise-returner
+* `NetworkLayer.METHODS`: dictionary/key-value map of [request methods](#request-methods)
+
+## Terminology
+### `RouteDefinition` interface 
+A `RouteDefinition` defines a single resource endpoint. All properties are shown below:
+```javascript
+interface RouteDefinition {
+    acceptHeaders: string | undefined;
+    contentType: string | undefined;
+    url: Function;
+    authenticate: boolean | undefined;
+    method: string | undefined
+}
+```
+Explanation: 
+* `acceptHeaders`: maps to `headers["Accept"]`; defaults to `application/json`
+* `contentType`: maps to `headers["Content-Type"]`; defaults to `application/json;charset=utf-8`
+* `url`: *required* function that takes params and returns a string. See example in `How Do You Use It` section above
+* `authenticate`: If used, this tells the `ConfiguredRoute` to check your params in `request( ...).with(params)` for a 'token' key to map to `headers["Authorization"]` (as "Bearer: {{ params.token }}").
+* `method`: maps to `headers["method"]`; defaults to `GET`
+
+### `APIConfig` class
+This is the primary class you will instantiate with your `routes` object. You only need one instance, though you can instantiate as many as you wish.
+
+### `Request Methods`
+The following methods can be specified (as members of `NetworkLayer.METHODS`) when defining a route:
+```typescript
+const METHODS = {
+    POST: 'POST',
+    GET: 'GET',
+    DELETE: 'DELETE',
+    PATCH: 'PATCH',
+    PUT: 'PUT',
+};
+```
+
+## FAQs
+### Why would you use this Library?
+This is ideal when you need to centralize the management of your
+application's routes. This system means that if server endpoints change, you only
+need to modify where your endpoints are defined, instead of in every view that may have 
+been calling the server directly.
+
+### Why _shouldn't_ you use this Library?
+~~Because you know better~~ Probably because you don't need to solve/have never encountered the issue it purports to solve. 
--- src/index.js
+++ src/index.js
@@ -1,117 +1,23 @@
-/* interface RouteDefinition {
-    acceptHeaders: string | undefined;
-	contentType: string | undefined;
-	url: Function;
-    authenticate: boolean | undefined;
-    method: string | undefined
-} */
-const METHODS = {
-    POST: 'POST',
-    GET: 'GET',
-    DELETE: 'DELETE',
-    PATCH: 'PATCH',
-    PUT: 'PUT',
-}
-
-export default {
-    APIConfig,
-    METHODS
-}
+/** HTTP Request Verbs */
+import APIConfig from "./configured-api";
+import methods from "./methods";
 
 /**
- * - Creates and configures a Fetch request using an APIRoute object
+ * - Creates and configures a `Fetch` request using an APIRoute object
  * - Triggers the request and returns the JSON from the server
+ * @param {object} routes A key-value store of server endpoints. Each key in
+ * `routes` will become a method on the new `APIConfig` instance. The value of each
+ * key will be a `RouteDefinition` with one or more of the following properties:
+ *  - `acceptHeaders`: string | undefined;
+ *  - `contentType`: string | undefined;
+ *  - `url`: Function;
+ *  - `authenticate`: boolean | undefined;
+ *  - `method`: string | undefined
+ * @param {object} globalErrorHandler An error handler to run when any network request
+ * fails. The handler will receive the (`APIConfig` instance-) rejected promise as its
+ * only argument. It should also return a promise (resolve/rejected per implementation needs).
  */
-function APIConfig(routes /* : { [x: string]: RouteDefinition } */ ) {
+export default APIConfig;
 
-    if (!routes) throw new Error("Missing routes");
-    this.routes = routes;
-
-    const routeKeys = Object.keys(routes);
-
-    if (routeKeys.length === 0) throw new Error("APIConfig needs at least one valid route definition");
-
-    this.request = (key /* : string */ ) /* : ConfiguredRoute */ => {
-        const route = this.routes[key];
-        if (!route) throw new Error(`Route ${key} is not defined: check Routes initialization`)
-        return new ConfiguredRoute(route);
-    }
-
-    // Append route keys to object so accessibe as APIConfig.route(params).then(...);
-    Object.keys(routes).forEach(routeName => {
-        const route = this.routes[routeName];
-        this[routeName] = new ConfiguredRoute(route).with;
-    })
-
-    return this;
-}
-
-APIConfig.prototype.METHODS = METHODS;
-
-function ConfiguredRoute (route/* : RouteDefinition */) {
-    this.route = route;
-
-    // with = (params/* : {[x: string]: string} */) => {
-    this.with = params => {
-        // Get an object ready to make a request
-        const method = this.route.method || METHODS.GET;
-        const staticSuccessResponse = { resolved: true };
-        let url = this.route.url(params);
-
-        let headers = {
-            'Accept': this.route.acceptHeaders || 'application/json',
-            'Content-Type': this.route.contentType || 'application/json;charset=utf-8',
-        };
-
-        // Inject token
-        if (this.route.authenticate) {
-            if (!params.token) throw new Error(`Did not pass token to authenticate at url ${url}`);
-            headers.Authorization = `Bearer: ${params.token}`;
-        }
-
-        // Configure request
-        let requestConfig = {
-            method,
-            headers,
-            // Needed to prevent Safari from being a real dick about server redirect responses (303, 30x response code)
-            redirect: "manual"
-        };
-
-        // Configure request body
-        if (method !== METHODS.GET) {
-            let body = (params && params.body) || params;
-            requestConfig.body = JSON.stringify(body)
-        }
-
-        let fetchRequest = new Request(url, requestConfig)
-        let responseCode = -1;
-
-        // Return fetch Promise
-        return fetch(fetchRequest)
-            .then(data => {
-                // Fetch won't reject a response unless there's a network failure, so handle that here
-                responseCode = data.status;
-                // Safari apparently handles API "redirect" (303, 30x) responses very, very poorly;
-                // We intercept the response and send something that doesn't cause the app to sink into depression
-                const responseIsAPIredirect = (data.type === "opaqueredirect");
-                if (responseIsAPIredirect) return staticSuccessResponse;
-                // "DELETE" request doesn't return a body, so return a boolean
-                const responseForDeleteRequest = (method === METHODS.DELETE);
-                if (responseForDeleteRequest && responseCode < 400) return staticSuccessResponse;
-                // At this point, the response *better* have a body. Or else.
-                // if content-type is json, return json
-                if (headers.Accept === 'application/json') return data.json();
-                return data;
-            })
-            // Check for API failures and reject response if response status error
-            .then(response => response.hasOwnProperty("error") ?
-                Promise.reject(response) :
-                Promise.resolve(response || staticSuccessResponse))
-            .catch(data => (responseCode > -1 && responseCode < 400) ? // Resolve response errors
-                Promise.resolve(staticSuccessResponse) :
-                Promise.reject(data)
-            );
-    }
-
-    return this;
-}
+/* HTTP Request methods */
+export const METHODS = methods;
